80133a9f16d59123fb025e51a0aeb2c8e681fa07
sofiadelgado-08 <sofiadelgadolovaton@gmail.com>
2024-10-15 01:07:13 -0400

    Delete L24.1.p.c2.Proyectico.27586641-28493285-Boxcinemas-Ejercicio.1 directory

--- L24.1.p.c2.Proyectico.27586641-28493285-Boxcinemas-Ejercicio.1/js/Familia.js
+++
@@ -1,8 +0,0 @@
-export default class Cl_mFamilia {
-    constructor ({familia, personas, dia, precioEntrada}){
-        this.familia = familia;
-        this.personas = personas;
-        this.dia = dia;
-        this.precioEntrada = 3; //precio base de la entrada
-    }
-}
--- L24.1.p.c2.Proyectico.27586641-28493285-Boxcinemas-Ejercicio.1/js/controlador.js
+++
@@ -1,9 +0,0 @@
-import Cl_mFamilia from "./Familia";
-
-export default class Cl_controlador {
-    constructor() {
-        this.mCine = new Cl_mCine();
-        this.vFamilia = new Cl_vFamilia(this);
-        this.vCine = new Cl_vCine(this);
-    }
-}
